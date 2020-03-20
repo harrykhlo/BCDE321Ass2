29145d23cc9a51a983dbf84b2ecd519272c086de
Harry <harrykhlo@gmail.com>
2020-03-20 21:57:03 +1300

    Harry made class_dia_cli.py robusthess

--- class_dia_cli.py
+++ class_dia_cli.py
@@ -3,17 +3,30 @@ import argparse
 import webbrowser
 import sys
 
-def run(args):
-    my_cli = MyCli(args.letter)
-    my_cli.cmdloop()
 
+# Harry's work
+def run(args):
+    my_cli = MyCli(args.letter[0])
+    try:
+        my_cli.cmdloop()
+    except Exception as err:
+        print("Please try again! The exception is: ", err)
+
+
+# Harry's work
 def main():
-    parser = argparse.ArgumentParser(description="This is a program going to a CLI to generate UML class diagram from Source Codes")
-    parser.add_argument("-l", help="optional: give a letter displaced at the beginning of each command line",
-                        dest="letter", type=str, default=">")
+    parser = argparse.ArgumentParser(description=
+                                     "This is a program going to a CLI to generate " +
+                                     "UML class diagram from Source Codes")
+    parser.add_argument("-l", help="optional: give a letter displaced at the beginning of each " +
+                                   "command line. If user enter a string, only first character " +
+                                   "will be shown.", dest="letter", type=str, default=">")
+
     parser.set_defaults(func=run)
     args = parser.parse_args()
     args.func(args)
 
+
+# Harry's work
 if __name__ == '__main__':
     main()
--- my_cli.py
+++ my_cli.py
@@ -3,25 +3,26 @@ import subprocess
 from file_to_data import FileToData
 import webbrowser
 
+
 class MyCli(Cmd):
     """Command line interpreter for generating UML class diagram"""
 
-    def __init__(self, my_name = ">"):
+    def __init__(self, my_name=">"):
         Cmd.__init__(self, my_name)
         self.my_name = my_name
         self.prompt = ">>" + my_name + ">> "
         self.file_to_data = FileToData()
 
-    def do_pyrclassdiagram(self, file_names):
+    def do_pyr_class_diagram(self, file_names):
         """Generate a class diagram in png format from given [png_file_name_suffix py_file_name.py]"""
         self.file_names = file_names
         pyreverse_command = 'pyreverse -ASmn -o png -p ' + file_names
         subprocess.call(pyreverse_command)
         print(file_names + ' are done')
 
-    def help_pyrclassdiagram(self):
+    def help_pyr_class_diagram(self):
         print("\n".join(['Generate a class diagram in png format from given file',
-        'classdiagram [output png file name suffix] [input source code file name.py])']))
+                         'class diagram [output png file name suffix] [input source code file name.py])']))
 
     def do_readsourcefile(self, file_name):
         """This function is incomplete. Harry is still working on this"""
@@ -39,13 +40,12 @@ class MyCli(Cmd):
         self.file_to_data.show_all_classes()
 
     def do_dot_2_png(self, input_dot_file_name):
-        #self.file_names = file_name
-        #url = "http://docs.python.org/library/webbrowser.html"
-        #chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
-        dot_command = 'dot -Tpng ' + input_dot_file_name + ' -o ' + input_dot_file_name +'.png'
+        # self.file_names = file_name
+        # url = "http://docs.python.org/library/webbrowser.html"
+        # chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+        dot_command = 'dot -Tpng ' + input_dot_file_name + ' -o ' + input_dot_file_name + '.png'
         subprocess.call(dot_command)
-        print(input_dot_file_name +'.png ' + ' are done')
-
+        print(input_dot_file_name + '.png ' + ' are done')
 
     def do_quit(self, line):
         """Exit this command line interpreter"""
@@ -54,6 +54,7 @@ class MyCli(Cmd):
 
     def help_quit(self):
         print("\n".join(['Quit from this CLI', ':return: True']))
+
 
 if __name__ == '__main__':
     my_cli = MyCli()
