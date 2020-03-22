264d8e1f2539973e72b96e3e7b361b62a1084cd7
Harry <harrykhlo@gmail.com>
2020-03-22 15:57:17 +1300

    added doctests into file_to_data.py

--- file_to_data.py
+++ file_to_data.py
@@ -9,7 +9,18 @@ from pprint import pprint
 
 # Harry's work
 class FileToData(ast.NodeVisitor):
+    """doctest
+    >>> file_to_data = FileToData()
+    >>> file_to_data.read_file("test.py")
+    >>> file_to_data.show_ast_nodes()
+    Module(body=[ClassDef(name='Car', bases=[], keywords=[], body=[FunctionDef(name='__init__', args=arguments(args=[arg(arg='self', annotation=None), arg(arg='num', annotation=None)], vararg=None, kwonlyargs=[], kw_defaults=[], kwarg=None, defaults=[]), body=[Assign(targets=[Attribute(value=Name(id='self', ctx=Load()), attr='door', ctx=Store())], value=Call(func=Name(id='Door', ctx=Load()), args=[Num(n=1)], keywords=[])), Assign(targets=[Attribute(value=Name(id='self', ctx=Load()), attr='wheel', ctx=Store())], value=Call(func=Name(id='Wheel', ctx=Load()), args=[Num(n=1)], keywords=[]))], decorator_list=[], returns=None), FunctionDef(name='is_sold', args=arguments(args=[arg(arg='self', annotation=None)], vararg=None, kwonlyargs=[], kw_defaults=[], kwarg=None, defaults=[]), body=[Expr(value=Call(func=Name(id='print', ctx=Load()), args=[Str(s='this car is sold')], keywords=[]))], decorator_list=[], returns=None)], decorator_list=[]), ClassDef(name='Door', bases=[], keywords=[], body=[FunctionDef(name='__init__', args=arguments(args=[arg(arg='self', annotation=None), arg(arg='num', annotation=None)], vararg=None, kwonlyargs=[], kw_defaults=[], kwarg=None, defaults=[]), body=[Assign(targets=[Attribute(value=Name(id='self', ctx=Load()), attr='number', ctx=Store())], value=Name(id='num', ctx=Load()))], decorator_list=[], returns=None)], decorator_list=[]), ClassDef(name='Wheel', bases=[], keywords=[], body=[FunctionDef(name='__init__', args=arguments(args=[arg(arg='self', annotation=None), arg(arg='num', annotation=None)], vararg=None, kwonlyargs=[], kw_defaults=[], kwarg=None, defaults=[]), body=[Assign(targets=[Attribute(value=Name(id='self', ctx=Load()), attr='number', ctx=Store())], value=Name(id='num', ctx=Load()))], decorator_list=[], returns=None)], decorator_list=[]), ClassDef(name='Taxi', bases=[Name(id='Car', ctx=Load())], keywords=[], body=[FunctionDef(name='__init__', args=arguments(args=[arg(arg='self', annotation=None)], vararg=None, kwonlyargs=[], kw_defaults=[], kwarg=None, defaults=[]), body=[Expr(value=Call(func=Name(id='super', ctx=Load()), args=[], keywords=[])), Assign(targets=[Attribute(value=Name(id='self', ctx=Load()), attr='color', ctx=Store())], value=Str(s='red'))], decorator_list=[], returns=None)], decorator_list=[])])
 
+    >>> file_to_data = FileToData()
+    >>> file_to_data.read_file("test.p")
+    Your given python file does not exist in the current directory or your input arguments were wrong. The file name should be [py_file_name.py]. Please try again!
+    """
+
+    # Harry's work
     def generic_visit(self, node):
         print(type(node).__name__)
         ast.NodeVisitor.generic_visit(self, node)
@@ -29,9 +40,11 @@ class FileToData(ast.NodeVisitor):
         except Exception as err:
             print("Please try again! The exception is: ", err)
 
+    # Harry's work
     def show_ast_nodes(self):
         print(ast.dump(self.tree))
 
+    # Harry's work
     def show_all_classes(self):
         # print(ast.dump(self.tree))
         for my_class in self.tree.body:
@@ -42,12 +55,17 @@ class FileToData(ast.NodeVisitor):
         #     print(len(tree.body)) # show 4 classes
 
 
-# below is for manual testing only
+    # Harry's work
 if __name__ == "__main__":
-    file_to_data = FileToData()
-    file_to_data.readfile("test.py")
-    file_to_data.show_ast_nodes()
-    file_to_data.show_all_classes()
+    # ---below is for manual testing only
+    # file_to_data = FileToData()
+    # file_to_data.read_file("test.p")
+    #file_to_data.show_ast_nodes()
+    # file_to_data.show_all_classes()
+    # ^^^below is for manual testing only
+    import doctest
+    doctest.testmod()
+
 
 # with open("test.py", "r") as source:
 #     # print(source.read())
